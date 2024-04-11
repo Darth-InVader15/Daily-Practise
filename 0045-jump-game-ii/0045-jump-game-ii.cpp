ee0436c5a1c4c875233495b0f825e6bb39d0179c
Piyush <95350374+Darth-InVader15@users.noreply.github.com>
2024-04-11 18:31:25 +0530

    Time: 13 ms (45.46%), Space: 19 MB (60.77%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,26 +1,5 @@
 class Solution {
-    //----------------DP--------------------
-// private:
-//     int solve(int ind, vector<int> &arr, vector<int> &dp)
-//     {
-//         if(ind >= arr.size())
-//             return 1e9;
-//         if(ind == arr.size()-1)
-//             return 0;
-        
-//         if(dp[ind] != -1)
-//             return dp[ind];
-        
-//         int cnt = 1e9;
-
-//         for(int i=1;i<=arr[ind];i++)
-//         {
-//             int steps = 1 + solve(ind+i,arr,dp);
-
-//             cnt = min(cnt,steps);
-//         }
-//         return dp[ind] = cnt;
-//     }
+    
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
