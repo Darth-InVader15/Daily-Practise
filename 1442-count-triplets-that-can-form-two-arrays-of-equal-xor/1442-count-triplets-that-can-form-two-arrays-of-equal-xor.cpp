bad89f4a294f2fb4bdae3392a289705bc6086a82
Piyush <95350374+Darth-InVader15@users.noreply.github.com>
2024-06-01 19:25:13 +0530

    Time: 4 ms (60.86%), Space: 9.1 MB (50.06%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
+//brute force 
     int countTriplets(vector<int>& arr) {
-        int count = 0;
+        int ans = 0;
         int n = arr.size();
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int a = 0;
-                for (int k = i; k < j; k++) {
-                    a ^= arr[k];
-                }
-                
-                int b = 0;
-                for (int k = j; k < n; k++) {
-                    b ^= arr[k];
-                    if (a == b) {
-                        count++;
-                    }
+
+        for(int i=0;i<n;i++)
+        {
+            int num = arr[i];
+
+            for(int k=i+1;k<n;k++) // We creating the sliding window
+            {
+                num ^= arr[k];
+                if(num == 0)    //we have encountered a case of 2 same xors
+                {
+                    ans += k-i; // this is because a^(b^c) == a^b^c 
+                    //so the location of j is not imp
                 }
             }
         }
-        
-        return count;
+        return ans;
     }
 };
