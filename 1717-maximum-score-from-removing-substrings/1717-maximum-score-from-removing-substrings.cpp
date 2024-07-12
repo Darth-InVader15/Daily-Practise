62c9e90d0317bec64f95e0d3488dfb951f2058b3
Piyush <95350374+Darth-InVader15@users.noreply.github.com>
2024-07-13 03:05:03 +0530

    Time: 224 ms (6.75%), Space: 28.4 MB (25%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
@@ -1,34 +1,79 @@
 class Solution {
 public:
     int maximumGain(string s, int x, int y) {
-        int aCount = 0;
-        int bCount = 0;
-        int lesser = min(x, y);
-        int result = 0;
+        stack<char> stk;
+        int n = s.size();
+        int ans = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(stk.empty()) stk.push(s[i]); //either empty stack or not ab forming elements
+            else
+            {
+                string temp = "";
+                temp.push_back(stk.top());
+                temp.push_back(s[i]);
 
-        for (char c : s) {
-            if (c > 'b') {
-                result += min(aCount, bCount) * lesser;
-                aCount = 0;
-                bCount = 0;
-            } else if (c == 'a') {
-                if (x < y && bCount > 0) {
-                    bCount--;
-                    result += y;
-                } else {
-                    aCount++;
+                if(x>=y) // remove all "ab"
+                {
+                    if(temp == "ab")
+                    {
+                        ans += x;
+                        stk.pop();
+                        continue;
+                    }
                 }
-            } else {
-                if (x > y && aCount > 0) {
-                    aCount--;
-                    result += x;
-                } else {
-                    bCount++;
+                else
+                {
+                    if(temp == "ba")
+                    {
+                        stk.pop();
+                        ans += y;
+                        continue;
+                    }
                 }
+                stk.push(s[i]);
             }
         }
+        //we have removed the majority string
+            string str = "";
+            while(stk.size())
+            {
+                str.push_back(stk.top());
+                stk.pop();
+            }
+            reverse(str.begin(),str.end());
+        
+        for(int i=0;i<str.size();i++)
+        {
+            if(stk.empty()) stk.push(str[i]); //either empty stack or not ab forming elements
+            else
+            {
+                string temp = "";
+                temp.push_back(stk.top());
+                temp.push_back(str[i]);
 
-        result += min(aCount, bCount) * lesser;
-        return result;
+                if(x>=y) // remove all "ab"
+                {
+                    if(temp == "ba")
+                    {
+                        ans += y;
+                        stk.pop();
+                        continue;
+                    }
+                }
+                else
+                {
+                    if(temp == "ab")
+                    {
+                        stk.pop();
+                        ans += x;
+                        continue;
+                    }
+                }
+                stk.push(str[i]);
+            }
+        }
+        return ans;
+
     }
 };
