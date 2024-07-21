c14c99c73e036ce742ea3c98b19c803800418a53
Piyush <95350374+Darth-InVader15@users.noreply.github.com>
2024-07-21 15:51:33 +0530

    Time: 40 ms (86.69%), Space: 48.4 MB (32.43%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,68 +1,74 @@
-struct Node {
-	Node *links[26];
-	bool flag = false;
-	//checks if the reference trie is present or not
-	bool containKey(char ch) {
-		return (links[ch - 'a'] != NULL);
-	}
-	//creating reference trie
-	void put(char ch, Node *node) {
-		links[ch - 'a'] = node;
-	}
-	//to get the next node for traversal
-	Node *get(char ch) {
-		return links[ch - 'a'];
-	}
-	//setting flag to true at the end of the word
-	void setEnd() {
-		flag = true;
-	}
-	//checking if the word is completed or not
-	bool isEnd() {
-		return flag;
-	}
+class Node{
+    public:
+    Node* links[26];
+    bool flag= false;
+
+    bool contains(char ch)
+    {
+        return links[ch-'a'] != NULL ;
+    }
+    void put(char ch, Node* node)
+    {
+        links[ch-'a'] = node;
+    }
+    Node* get(char ch)
+    {
+        return links[ch-'a'];
+    }
+    void setEnd()
+    {
+        flag = 1;
+    }
+    bool isEnd()
+    {
+        return flag;
+    }
 };
 class Trie {
-private:
-	Node* root;
 public:
-	Trie() {
-		//creating new obejct
-		root = new Node();
-	}
-
-	void insert(string word) {
-		//initializing dummy node pointing to root initially
-		Node *node = root;
-		for (int i = 0; i < word.size(); i++) {
-			if (!node->containKey(word[i])) {
-				node->put(word[i], new Node());
-			}
-			//moves to reference trie
-			node = node->get(word[i]);
-		}
-		node->setEnd();
-	}
-
-	bool search(string word) {
-		Node *node = root;
-		for (int i = 0; i < word.size(); i++) {
-			if (!node->containKey(word[i])) {
-				return false;
-			}
-			node = node->get(word[i]);
-		}
-		return node->isEnd();
-	}
-
-	bool startsWith(string prefix) {
-		Node* node = root;
-		for (int i = 0; i < prefix.size(); i++) {
-			if (!node->containKey(prefix[i])) {
-				return false;
-			}
-			node = node->get(prefix[i]);
-		}
-		return true;
-	}
+    Node* root;
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node* temp = root;
+        for(auto x:word)
+        {
+            if(!temp->contains(x))
+            {
+                temp->put(x,new Node());
+            }
+            temp = temp->get(x);
+        }
+        temp->setEnd();
+    }
+    
+    bool search(string word) {
+        Node* temp = root;
+        for(auto x:word)
+        {
+            if(!temp->contains(x))   return false;
+            temp = temp->get(x);
+        }
+        return temp->isEnd();
+    }
+    
+    bool startsWith(string word) {
+        Node* temp= root;
+        for(auto x:word)
+        {
+            if(!temp->contains(x))   return 0;
+            temp = temp->get(x);
+        }
+        return 1;
+    }
 };
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
