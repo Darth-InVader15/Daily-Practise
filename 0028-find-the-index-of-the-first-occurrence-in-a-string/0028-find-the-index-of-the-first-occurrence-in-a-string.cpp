a157da7e411873de57c7d816a675d55fdeb06fea
Piyush <95350374+Darth-InVader15@users.noreply.github.com>
2024-08-01 12:34:09 +0530

    Time: 0 ms (100%), Space: 8 MB (23.56%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,55 +1,62 @@
-class Solution{
-private:
-    const int mod = 1e9 + 7;
-    const int base = 31;
+class Solution {
+    vector<int> preprocess(string &s)
+    {
+        vector<int> kmp(s.size());
+        int i = 1, m = 0;
+        kmp[0] = 0;
 
-    //Rolling Hash aka Rabin Karp
-    // I tried a solution after watching a TLE eliminators videos
-    //couldnt handle the overflow for the exponential power
-
-    
-    long long hash(string &str) {
-        long h = 0;
-        int n = str.size();
-        for(int i = 0; i < n; i++) {
-            h = (h * base + (str[i] - 'a' + 1)) % mod;
-        }
-        return h;
-    }
-
-    long long modExp(long long base, long long exp, long long mod) {
-        long long result = 1;
-        while(exp > 0) {
-            if(exp % 2 == 1) {
-                result = (result * base) % mod;
+        while(i<s.size())
+        {
+            if(s[i] == s[m])
+            {
+                kmp[i++] = ++m;
             }
-            base = (base * base) % mod;
-            exp /= 2;
+            else if(m>0)    m = kmp[m-1];
+            else kmp[i++] = 0;
         }
-        return result;
+        return kmp;
+
     }
-
 public:
-    int strStr(string hay, string need) {
-        int n = hay.size();
-        int m = need.size();
-        if(m > n) return -1;
+    int strStr(string haystack, string needle) {
+        vector<int> ind(26,0);
+        // vector<pair<char,int>> dict(needle.size()+1);
+        int n = haystack.size(), m = needle.size();
 
-        if(need == hay) return 0;
-        
-        int needHash = hash(need);
-        string temp = hay.substr(0, m);
-        int hayHash = hash(temp);
-        if(hayHash == needHash && hay.substr(0, m) == need) return 0;
+        vector<int> kmp = preprocess(needle);
 
-        long long basePower = modExp(base, m - 1, mod);
-
-        for(int i = m; i < n; i++) {
-            hayHash = (hayHash - (hay[i - m] - 'a' + 1) * basePower % mod + mod) % mod;
-            hayHash = (hayHash * base + (hay[i] - 'a' + 1)) % mod;
-            if(hayHash == needHash && hay.substr(i - m + 1, m) == need) return i - m + 1;
+        // for(int i=0;i<needle.size();i++)
+        // {
+        //     dict[i+1] = {needle[i],ind[needle[i]-'a']}; //we saving the earlier occurence
+        //     if(ind[needle[i]-'a'] == 0) ind[needle[i]-'a'] = i+1;
+        // }
+        int j = 0, i =0;
+        while(i<n && j<m)
+        {
+            while(i<n && j<m && haystack[i] == needle[j])
+            {
+                i++;
+                j++;
+                if(j == m)
+                {
+                    return i-m;
+                } //match found
+                if(i == n)  return -1; //string over
+            }
+            if(haystack[i] != needle[j])
+            {
+                if(j == 0)
+                {
+                    i++;
+                }
+                else
+                {
+                    j = kmp[j-1]; //jo pichla match kara tha
+                    
+                }
+                continue;
+            }
         }
-
         return -1;
     }
 };
